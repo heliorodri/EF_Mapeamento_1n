db79b9d51780fc84ac129da39d12b916b9a4cbe0
Hélio Junior Rodrigues <helio97jr@hotmail.com>
2019-11-03 14:46:19 -0300

    First Commit

---
+++ Mapeamento_1_N.Domain/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mapeamento_1_N.Domain
+{
+    [Table("Produtos")]
+    class Produto
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
